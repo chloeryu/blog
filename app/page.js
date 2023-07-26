f4f04d27489641c1932e0ed42fa6854b1a81d3de
chloeryu <yeonhee8012@gmail.com>
2023-07-26 23:17:56 +0900

    변수

--- app/page.js
+++ app/page.js
@@ -6,16 +6,15 @@ export default function Home() {
   let 책 = ['구글애드센스로 돈벌기', '깃허브입문', '프론트엔드 웹 디자인 입문']
   return (
     <div>
-      <h4 className="title"></h4>
-      <div className="book">
-        <h4>{책[0]} $40</h4>
-      </div>
-      <div className="book">
-        <h4>{책[1]} $40</h4>
-      </div>
-      <div className="book">
-        <h4>{책[2]} $40</h4>
-      </div>
+      <h4 className="title">상품목록</h4>
+      {
+        책.map(() => {
+          return (
+            <div className="book">
+              <h4>{책[0]} $40</h4>
+            </div>
+          )
+        })}
 
     </div>
   )
