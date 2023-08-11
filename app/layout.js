26f12c80a27ec4b6b1d0013b4d0057ff5650d3de
chloeryu <yeonhee8012@gmail.com>
2023-08-11 12:55:06 +0900

    23.8.11

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,7 @@ export default function RootLayout({ children }) {
             <div className="search" >
               <input type="text" placeholder="Search" />
             </div>
+
           </div>
 
 
